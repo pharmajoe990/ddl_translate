b52a8c0a6ad575e5db2834acba3cffd7169e1d02
Timothy Roper <tim@Timothys-MacBook-Pro.local>
2013-09-13 16:42:26 +1000

    Added Error checking for datatype on SQL Server

--- SQLServerColumn.py
+++ SQLServerColumn.py
@@ -9,15 +9,24 @@ class SQLServerColumn(object):
 	precision = ""
 	isNullable = False
 
+		#All SQL Server datatypes. Used for error checking mapping translations between DBMSes
+	dataTypes = ('numeric', 'varchar', 'char', 'nvarchar', 'money', 'datetime', 'int', 'decimal', 'float', 'uniqueidentifier');
+
 	def __init__(self, name, dataType, length, precision, isNullable):
 		self.name = name
-		self.dataType = dataType
 		self.length = length
 		self.precision = precision
 		self.isNullable = isNullable
+		setType(dataType)
 
 	def getAttributes(self):
 		"""Build dictionary of getAttributes"""
 		return dict(name=self.name, dataType=self.dataType, length=str(self.length), precision=str(self.precision), nullable=str(self.isNullable))
 
-	
+	def checkDataType(self, dataType):
+		"""Validates Datatypes are allowable"""
+		if dataType not in self.datatypes
+			#Invalid dataType
+			#TODO Implement exception
+			print dataType, 'not valid MSSQL type'
+		self.type = dataType
