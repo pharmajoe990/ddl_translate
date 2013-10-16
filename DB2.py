7573478abb20c526696538770625859d5f8b0711
Timothy Roper <tim@Timothys-MacBook-Pro.local>
2013-10-16 19:24:18 +1100

    Ready to push to bitbucket

---
+++ DB2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class DB2Generator(object):
+	"""Translate from standard XML into"""
+	
+	def __init__(self, arg):
+		super(DB2Generator, self).__init__()
+		self.arg = arg
+		
+
+# def translate():
+	
+	
